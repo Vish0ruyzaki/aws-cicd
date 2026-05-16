4fad0315011fdfcab40554cc845f02fd9762c03b
Vish0ruyzaki <vishwajeetsingh62061@gmail.com>
2026-05-16 11:33:31 +0530

    updated code

--- src/main/java/com/javatechie/dto/Course.java
+++ src/main/java/com/javatechie/dto/Course.java
@@ -1,15 +1,35 @@
 package com.javatechie.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Course {
 
     private int id;
     private String name;
     private double price;
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for price
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
